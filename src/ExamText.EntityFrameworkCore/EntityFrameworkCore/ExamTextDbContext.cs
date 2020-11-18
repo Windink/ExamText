6139b89b7d79e6d9d2046d85390847a3d512b297
ink Wind <1551146275@qq.com>
2020-11-18 15:16:19 +0800

    添加项目文件。

---
+++ src/ExamText.EntityFrameworkCore/EntityFrameworkCore/ExamTextDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using ExamText.Authorization.Roles;
+using ExamText.Authorization.Users;
+using ExamText.MultiTenancy;
+
+namespace ExamText.EntityFrameworkCore
+{
+    public class ExamTextDbContext : AbpZeroDbContext<Tenant, Role, User, ExamTextDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public ExamTextDbContext(DbContextOptions<ExamTextDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
